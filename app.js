84724e659e7e81ce536724e7181d5f9acb6b6e86
unknown <you@example.com>
2021-06-18 18:28:48 +0500

    class 9

---
+++ app.js
@@ -0,0 +1,45 @@
+//alert ("hello world");
+
+//var a = 5;
+//var b = 10;
+//var c = a+b;
+//alert (c)
+
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// console.log (c);
+
+// var x = "nabeel";
+// var y = "saimoon"
+// var z = y+x;
+// alert  (z);
+
+// var f = 20;
+// var h = "10";
+// var g = f+h;
+// alert (g);
+
+// var f = 20;
+// var h = "10";
+// var g = f*h;
+// alert (g);
+
+// var a = 20;
+// var b = 10;
+// var c = 20;
+// var d = a - b * c
+// alert (d);
+
+// var a = 6;
+// var b = a++;
+// alert (b);
+
+// var a = 6;
+// var b = ++a;
+// alert (b);
+
+var x = 7;
+var y = x++ + ++x -x x++ - ++x +x;
+alert (y)
